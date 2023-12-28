d961f8f1a66b161d752c17454210314967546ef9
Артём Тюрин <zientenin@mail.ru>
2023-12-29 01:41:13 +0300

    Update task 12

--- 12.py
+++ 12.py
@@ -3,30 +3,23 @@
 Выполнить их слияние так, чтобы полученный список так же был сортирован.
 """
 
-def merge_two_list(a: list, b: list):
-    c = []
-    i, j = 0, 0
-    while i < len(a) and j < len(b):
+def merge_sort(a: list, b: list):
+    i, j, c = 0, 0, []
+    while True:
         if a[i] < b[j]:
             c.append(a[i])
             i += 1
+            if i == len(a):
+                c.extend(b[j:])
+                break
         else:
             c.append(b[j])
             j += 1
-    if i < len(a):
-        c += a[i:]
-    if j < len(b):
-        c += b[j:]
+            if j == len(b):
+                c.extend(a[i:])
+                break
     return c
 
-# def merge_sort(arr: list):
-#     l = len(arr)
-#     if l == 1:
-#         return arr
-#     middle = l // 2
-#     left = merge_sort(arr[:middle])
-#     right = merge_sort(arr[middle:])
-#     return merge_two_list(left, right)
 
-# print(merge_two_list([12, 23, 37, 64], [15]))
+print(merge_sort([12, 23, 37, 64], [15, 38]))
     
